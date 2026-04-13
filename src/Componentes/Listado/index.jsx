0ebbfee670124b024a831dad9f27664e04d2c54c
Manuel <manuelmandel09@gmail.com>
2026-04-13 09:57:06 -0300

    Remove citas prop and conditional rendering from Listado component

--- src/Componentes/Listado/index.jsx
+++ src/Componentes/Listado/index.jsx
@@ -1,26 +1,10 @@
 import Cita from "../Cita"
 
-function Listado({ citas = [] }) {
-    return (
+function Listado(){
+    return(
+
         <div>
             <h2>Administra tus citas</h2>
-
-            {citas.length === 0 ? (
-                <p>No hay citas aún</p>
-            ) : (
-                citas.map((cita) => (
-                    <Cita
-                        key={cita.id}
-                        Mascota={cita.mascota}
-                        Dueño={cita.propietario}
-                        Fecha={cita.fecha}
-                        Hora={cita.hora}
-                        Sintomas={cita.sintomas}
-                    />
-                ))
-            )}
         </div>
     )
 }
-
-export default Listado
